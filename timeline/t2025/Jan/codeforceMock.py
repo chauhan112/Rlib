abb10d6b20fd43eb8e21762abebe7cc1cde1a766
raja <rajababuchauhan500@gmail.com>
2025-05-30 09:44:39 +0200

    simple mock for codeforce to code faster

---
+++ timeline/t2025/Jan/codeforceMock.py
@@ -0,0 +1,13 @@
+from basic import Main as ObjMaker
+def MockTestCodeForces():
+    lines = []
+    def set_text(text):
+        s.process.lines = text.strip().splitlines()
+        s.process.index = 0
+    def inputt():
+        s.process.index += 1
+        return s.process.lines[s.process.index-1]
+    def reset():
+        s.process.index = 0
+    s = ObjMaker.variablesAndFunction(locals())
+    return s
