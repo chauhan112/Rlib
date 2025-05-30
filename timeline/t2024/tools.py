24da01b3d9b87f5dde2385afc0a1df043408015c
raja <rajababuchauhan500@gmail.com>
2025-05-30 09:41:28 +0200

    new year related tools

--- timeline/t2024/tools.py
+++ timeline/t2024/tools.py
@@ -18,3 +18,24 @@ def FileTools():
         return conversionTool.handlers.sizeReduce(sum(map(File.size, filepaths)))
     s = ObjMaker.variablesAndFunction(locals())
     return s
+def YearManagerTools():
+    import os
+    import calendar
+    import datetime
+    from SerializationDB import SerializationDB
+    from LibsDB import LibsDB
+    from timeline.t2023.tools import TimelineDB
+
+    def createIpynbFiles(year):
+        months = list(calendar.month_name)[1:]
+        short_months_with_indices = [ '{:0>2d}'.format(i+1) + "_" + val[:3] for i, val in enumerate(months)]
+        year_loc = TimelineDB.getYearPath(year)
+        month_fullpaths = [year_loc + os.sep + mth for mth in short_months_with_indices]
+        for dirpath in month_fullpaths:
+            if not os.path.exists(dirpath):
+                os.makedirs(dirpath)
+            spaceIpynb = dirpath + os.sep + "space.ipynb" 
+            content = SerializationDB.readPickle(LibsDB.picklePath("temps"))["timeline"]["2024"]["12_Dec"]["nb-data"]
+            File.overWrite(spaceIpynb, content)
+    s = ObjMaker.variablesAndFunction(locals())
+    return s
