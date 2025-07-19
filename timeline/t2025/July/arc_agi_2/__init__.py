a44771992146916115f56e737cea116ff75be107
raja <rajababuchauhan500@gmail.com>
2025-07-19 14:38:26 +0200

    some standard tools in the main package

---
+++ timeline/t2025/July/arc_agi_2/__init__.py
@@ -0,0 +1,3 @@
+from .....basic import Main as ObjMaker
+from .tools import ColorMap, Labels
+from .solver import sol_00576224, sol_007bbfb7, Solver009d5c81, sol_00d62c1b, Solver00dbd492, Solver017c7c7b, sol_025d127b, sol_03560426
