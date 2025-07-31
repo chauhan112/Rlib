c2ed14d7c1c17c9ed2f93bda6898438721637d06
raja <rajababuchauhan500@gmail.com>
2025-07-31 20:15:26 +0200

    copy bug fix code

---
+++ tests/dailyCode/t2025/July/copyDirListBugFix.py
@@ -0,0 +1,12 @@
+# %%
+from useful.jupyterDB import jupyterDB
+jupyterDB._params = globals()
+from useful.CSS import Main as CSSMain
+CSSMain.loadInCssComponent()
+# %%
+import os
+from timeline.t2024.generic_logger.generic_logger_v6 import GenericLoggerV6
+from useful.LibsDB import LibsDB
+filename = os.sep.join([LibsDB.cloudPath(), 'timeline', '2024', '10_Oct', "abc.sqlite"])
+gl = GenericLoggerV6(filename)
+gl.views.container.outputs.layout
