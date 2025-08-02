cc56a3b11f0be843d5ed7c3509ea0063a0442e6f
raja <rajababuchauhan500@gmail.com>
2025-08-02 16:34:33 +0200

    api exposing for the db interface (code is extremely generalized)

---
+++ timeline/t2025/Aug/exposed_apis.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from .db import addData, deleteDataWithId, deleteDataWhere, readAllWithPagination, \
+    readAsDic, readWhere, updateData, readAll
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class Command(BaseModel):
+    name: str
+    params: list | None = None
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+@app.post("/run/")
+def run_cmd(command: Command):
+    params = command.params
+    if params is None:
+        params = []
+    return eval(command.name)(*params)
+
+def get_app():
+    return app
+def run_app():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn main:app --reload
+
